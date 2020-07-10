4aa2cee2d71a13227ad13f250ac4135ea37ac91c
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2020-07-10 16:10:05 +0200

    network based on proportions

---
+++ R/10_CyTOF_networks.R
@@ -0,0 +1,56 @@
+# Loading code
+source("/Users/senosam/Documents/Repositories/Research/data_analysis_cytof/R/20_ClustAnnot_functions.R")
+
+# Loading data
+load("/Users/senosam/Documents/Massion_lab/CyTOF_summary/both/cellsubtypes.RData")
+
+# Organizing data
+# All cell/sub types B have Fib_Mesenchymal
+ref <- ref[-42,] # sample 13376 has been collected twice and will be merged, only one ref is needed
+
+# Changing un-identified T cells into "Other_immune"
+k <- which(annot_df$subtype_B =='T_cells')
+k2 <- which(annot_df$subtype_B =='NK_cells')
+
+annot_df['subtype_B'] <- as.character(annot_df$subtype_B)
+annot_df[c(k,k2), 'subtype_B'] <- 'Other_immune'
+annot_df$subtype_B <- as.factor(annot_df$subtype_B)
+
+annot_df['subtype_B4'] <- as.character(annot_df$subtype_B)
+annot_df[which(annot_df$cell_type_B=='Epithelial'), 'subtype_B4'] <- 'Epithelial'
+annot_df$subtype_B4 <- as.factor(annot_df$subtype_B4)
+annot_df$subtype_B4 <- factor(annot_df$subtype_B4, levels = c("Epithelial", 
+    "Endothelial", "Fib_Mesenchymal", "Other_immune", "Myeloid", "Tc_cells", 
+    "Th_cells"))
+
+prcnt_by_pt1 <- ClassAbundanceByPt(data=annot_df, ptID_col = 'pt_ID', 
+    class_col = 'subtype_B')
+#rownames(prcnt_by_pt1) <- paste0(rownames(prcnt_by_pt1), '_', ref$CANARY)
+
+#compute correlation matrix
+corr_pt <- Hmisc::rcorr(t(as.matrix(prcnt_by_pt1)), type = 'spearman')
+corr_mat <- corr_pt$r
+# select only significant correlations
+corr_pt$P[which(is.na(corr_pt$P)==TRUE)] <- 0
+k <- which(corr_pt$P<0.05)
+corr_mat[-k] <- 0
+corr_mat[corr_mat==1] <-0
+# Keep only high correlations
+corr_mat[corr_mat<0.5] <- 0
+ 
+# Make an Igraph object from this matrix:
+network <- graph_from_adjacency_matrix(corr_mat, weighted=T, mode="undirected", diag=F)
+
+# Basic chart
+plot(network)
+
+E(network)$weight
+
+
+
+
+
+
+prcnt_by_pt2 <- ClassAbundanceByPt(data=annot_df, ptID_col = 'pt_ID', 
+    class_col = 'subtype_B4')
+#rownames(prcnt_by_pt2) <- paste0(rownames(prcnt_by_pt2), '_', ref$CANARY)
