9e50879f8dd424034917942719695e684999e5bb
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2020-07-16 11:17:03 +0200

    code structure

--- R/10_CyTOF_networks.R
+++ R/10_CyTOF_networks.R
@@ -1,13 +1,16 @@
-# Loading code
+####################-- LOAD LIBRARIES AND FUNCITONS--####################
 source("/Users/senosam/Documents/Repositories/Research/data_analysis_cytof/R/20_ClustAnnot_functions.R")
 library(RCy3)
 library(igraph)
 
-# Loading data
+
+############################-- LOAD DATA--############################
 load("/Users/senosam/Documents/Massion_lab/CyTOF_summary/both/cellsubtypes.RData")
+load("/Users/senosam/Documents/Massion_lab/CyTOF_summary/both/analysis/subsets/subset_exprmat.RData")
 pdata <- read.csv("/Users/senosam/Documents/Massion_lab/CyTOF_summary/CDE_TMA36_2020FEB25_SA.csv")
 
-# Organizing data
+
+############################-- ORGANIZING DATA--############################
 # All cell/sub types B have Fib_Mesenchymal
 ref <- ref[-42,] # sample 13376 has been collected twice and will be merged, only one ref is needed
 
@@ -28,50 +31,51 @@ annot_df$subtype_B4 <- factor(annot_df$subtype_B4, levels = c("Epithelial",
 
 prcnt_by_pt1 <- ClassAbundanceByPt(data=annot_df, ptID_col = 'pt_ID', 
     class_col = 'subtype_B')
+prcnt_by_pt2 <- ClassAbundanceByPt(data=annot_df, ptID_col = 'pt_ID', 
+    class_col = 'subtype_B4') # this is epi as one
 
-#compute correlation matrix
+
+############################-- BUILD NETWORK --############################
+# Compute correlation matrix
 corr_pt <- Hmisc::rcorr(t(as.matrix(prcnt_by_pt1)), type = 'spearman')
 corr_mat <- corr_pt$r
 
-# select only significant correlations
+# Select only significant correlations
 corr_pt$P[which(is.na(corr_pt$P)==TRUE)] <- 0
 k <- which(corr_pt$P<0.05)
 corr_mat[-k] <- 0
-
-# Keep only high correlations
-#corr_mat[corr_mat<0.5] <- 0
+#corr_mat[corr_mat<0.5] <- 0 # Keep only high correlations
  
 # Make an Igraph object from this matrix:
-network <- graph_from_adjacency_matrix(corr_mat, weighted=T, mode="undirected", diag=F)
+network <- graph_from_adjacency_matrix(corr_mat, weighted=T, mode="undirected", 
+    diag=F)
 E(network)$weight
 
 # Open Cytoscape and confirm connexion
 cytoscapePing()
 createNetworkFromIgraph(network,"cytof_ntw")
 
-# Append node attributes (clinical data) to network in Cytoscape
+
+###############-- APPEND NODE ATTRIBUTES TO NTW IN CYTOSCAPE --###############
+## Clincal data
 pdata <- data.frame(pdata, row.names = pdata$pt_ID, stringsAsFactors = FALSE)
 loadTableData(pdata)
 
-# Append node attributes (ct %) to network in Cytoscape
-prcnt_by_pt1 <- data.frame(prcnt_by_pt1, row.names = row.names(prcnt_by_pt1), stringsAsFactors = FALSE)
+## Cell type percentages
+prcnt_by_pt1 <- data.frame(prcnt_by_pt1, row.names = row.names(prcnt_by_pt1), 
+    stringsAsFactors = FALSE)
 loadTableData(prcnt_by_pt1)
 
-
-
-# protein expression
-## median protein expression per patient
-## median protein expression per cell type per patient
-## add this info to the nodes attributes
-
-load("/Users/senosam/Documents/Massion_lab/CyTOF_summary/both/analysis/subsets/subset_exprmat.RData")
-sbst_exp <- data.frame(sbst_exp, row.names = sbst_exp$pt_ID, stringsAsFactors = FALSE)
+## Median protein expression per cell type per patient
+sbst_exp <- data.frame(sbst_exp, row.names = sbst_exp$pt_ID, 
+    stringsAsFactors = FALSE)
 loadTableData(sbst_exp)
+#sbst_exp_2 # epithelial by subset
 
 
 
 
 
-prcnt_by_pt2 <- ClassAbundanceByPt(data=annot_df, ptID_col = 'pt_ID', 
-    class_col = 'subtype_B4')
-#rownames(prcnt_by_pt2) <- paste0(rownames(prcnt_by_pt2), '_', ref$CANARY)
+
+
+
