996882de98eaec0e28885d8376f924ea2828fe7a
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2020-10-07 22:48:46 +0200

    plan

---
+++ R/ML.R
@@ -0,0 +1,12 @@
+# CyTOF
+# get subtypes proportions (normed by tumor content)
+# get overall immune score (% of immune cells)
+# get protein expression per cell type
+
+# RNA
+# eigen genes (cluster scores from Clust)
+# gene expression of Clust slelected genes (500 genes)
+# Deconvolution results
+
+# WES
+# binary matrix for somatic mutations, remove genes that are mutated in less than 5% of samples
