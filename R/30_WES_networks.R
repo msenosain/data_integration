c6f913808c079fc740b26685ac96a0c9772b230c
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2020-10-23 17:49:04 +0200

    matrix 0 1

--- R/30_WES_networks.R
+++ R/30_WES_networks.R
@@ -10,3 +10,23 @@ clin_dt <- read.csv('/Users/senosam/Documents/Massion_lab/CyTOF_summary/CDE_TMA3
 clin_dt <- clin_dt[match(pt_ID, clin_dt$pt_ID),]
 clin_dt <- cbind('Tumor_Sample_Barcode'=x$Tumor_Sample_Barcode, clin_dt)
 mut = read.maf(maf = maf_dir, clinicalData = clin_dt)
+
+
+mut_dt <- mut@data
+
+h_sym <- unique(mut_dt$Hugo_Symbol)
+pt_ID <- unique(mut_dt$Tumor_Sample_Barcode)
+
+mut_df <- matrix(0, nrow=length(h_sym), ncol=length(pt_ID))
+rownames(mut_df) <- h_sym
+colnames(mut_df) <- pt_ID
+
+
+for (i in 1:length(h_sym)){
+    k <- which(mut_dt$Hugo_Symbol == h_sym[i])
+    pt <- mut_dt$Tumor_Sample_Barcode[k]
+    mut_df[i,pt] <- 1
+}
+
+colnames(mut_df) <- sapply(strsplit(as.character(pt_ID), "pt"), "[[", 2)
+colnames(mut_df) <- sapply(strsplit(colnames(mut_df), "_"), "[[", 1)
